f9b9143643e3ffd78129086e46f2b65ef5cad6da
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-16 17:53:54 -0300

    Commit inicial

---
+++ src/test/java/dev/unscrud/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.unscrud.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
