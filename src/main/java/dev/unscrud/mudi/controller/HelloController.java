2d2978b754bbe9292d20f9107e6bc340d724f50d
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-17 10:12:59 -0300

    o spring prove a interface model que pode ser usada no lugar do HttpServletRequest

--- src/main/java/dev/unscrud/mudi/controller/HelloController.java
+++ src/main/java/dev/unscrud/mudi/controller/HelloController.java
@@ -1,15 +1,15 @@
 package dev.unscrud.mudi.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     
     @GetMapping("/hello")
-    public String hello(HttpServletRequest request){
-        request.setAttribute("nome", "unscrud");
+    public String hello(Model model){
+        model.addAttribute("nome", "unscrud");
         return "hello";
     }
 }
