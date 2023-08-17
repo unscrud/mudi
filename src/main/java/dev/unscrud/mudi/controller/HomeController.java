16637f40abc82a8b29b6be69e77f984fb273f585
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-17 15:26:43 -0300

    set do valor e da url do produto

--- src/main/java/dev/unscrud/mudi/controller/HomeController.java
+++ src/main/java/dev/unscrud/mudi/controller/HomeController.java
@@ -14,8 +14,8 @@ public class HomeController {
         Pedido pedido = new Pedido();
         pedido.setNomeDoProduto("Teclado Mecânico Bora");
         pedido.setDataDaEntrega(LocalDate.now());
-        pedido.setValorNegociado(new BigDecimal("149,99"));
-        pedido.setUrlProdudo("https://www.amazon.com.br/Teclado-Mec%C3%A2nico-T-dagger-Bora-Switch/dp/B0833F1KQ3/ref=sr_1_6?keywords=teclado+mecanico&qid=1692286853&sprefix=TECLA%2Caps%2C407&sr=8-6&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d");
+        pedido.setValorNegociado(new BigDecimal("149.99"));
+        pedido.setUrlProduto("https://www.amazon.com.br/Teclado-Mec%C3%A2nico-T-dagger-Bora-Switch/dp/B0833F1KQ3/ref=sr_1_6?keywords=teclado+mecanico&qid=1692286853&sprefix=TECLA%2Caps%2C407&sr=8-6&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d");
         pedido.setUrlImagem("https://m.media-amazon.com/images/I/61j-3qqbAhL._AC_SX522_.jpg");
         pedido.setDescricao("T-DAGGER Teclado Mecânico Bora Rgb Switch - T-tgk315-bl, PRETO");
         
