cd2e1f1826019f606aeccde5786f1dad6c2282ac
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-18 18:42:07 -0300

    outra opção é o JpaRepository que possui alguns métodos além do CrudRepository que só fornece as operações básicas de crud

--- src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
+++ src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
@@ -1,8 +1,8 @@
 package dev.unscrud.mudi.repository;
 
 import dev.unscrud.mudi.model.Pedido;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PedidoRepository extends CrudRepository<Pedido, String>{}
+public interface PedidoRepository extends JpaRepository<Pedido, String>{}
