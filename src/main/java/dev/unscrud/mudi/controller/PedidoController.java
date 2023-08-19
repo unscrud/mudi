1f4deddf87822d3228aa78c002804edd29b4fe4a
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-19 19:46:03 -0300

    salvando no banco um novo pedido

--- src/main/java/dev/unscrud/mudi/controller/PedidoController.java
+++ src/main/java/dev/unscrud/mudi/controller/PedidoController.java
@@ -1,16 +1,33 @@
 package dev.unscrud.mudi.controller;
 
+import dev.unscrud.mudi.dto.NovoPedido;
+import dev.unscrud.mudi.model.Pedido;
+import dev.unscrud.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    
     @GetMapping("formulario")
     public ModelAndView formulario(){
         ModelAndView mv = new ModelAndView("pedido/formulario");
+        return mv;
+    }
+    
+    @PostMapping("novo")
+    public ModelAndView novo(NovoPedido novoPedido){
+        Pedido pedido = novoPedido.toPedido();
+        pedidoRepository.save(pedido);
+        
+        ModelAndView mv = new ModelAndView("home");
         return mv;
     }
 }
