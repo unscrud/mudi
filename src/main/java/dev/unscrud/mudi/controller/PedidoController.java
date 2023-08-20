ff502e1df20fe237513bfd0dd5a221fb48ac2fbb
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-20 15:30:04 -0300

    adição de validação da requisição vinda do form

--- src/main/java/dev/unscrud/mudi/controller/PedidoController.java
+++ src/main/java/dev/unscrud/mudi/controller/PedidoController.java
@@ -3,8 +3,10 @@ package dev.unscrud.mudi.controller;
 import dev.unscrud.mudi.dto.NovoPedido;
 import dev.unscrud.mudi.model.Pedido;
 import dev.unscrud.mudi.repository.PedidoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,18 @@ public class PedidoController {
     }
     
     @PostMapping("novo")
-    public ModelAndView novo(NovoPedido novoPedido){
+    public ModelAndView novo(@Valid NovoPedido novoPedido, BindingResult result){
+        ModelAndView mv;
+        
+        if (result.hasErrors()){
+            mv = new ModelAndView("pedido/formulario");
+            return mv;
+        }
+        
         Pedido pedido = novoPedido.toPedido();
         pedidoRepository.save(pedido);
         
-        ModelAndView mv = new ModelAndView("home");
+        mv = new ModelAndView("pedido/novo");
         return mv;
     }
 }
