8db293e3b19f517342c1d83486ccf8ee920ef009
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-22 20:47:07 -0300

    criação de uma busca por pedidos pelo status

--- src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
+++ src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
@@ -1,8 +1,13 @@
 package dev.unscrud.mudi.repository;
 
 import dev.unscrud.mudi.model.Pedido;
+import dev.unscrud.mudi.model.StatusPedido;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, String>{}
+public interface PedidoRepository extends JpaRepository<Pedido, String>{
+
+    public List<Pedido> findByStatusPedido(StatusPedido statusPedido);
+}
