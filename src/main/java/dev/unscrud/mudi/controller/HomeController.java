3e5f5bd4e976bacf56dd74cac8564453be5beb4a
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-22 20:48:00 -0300

    mapeamento do /home na classe para criar o aguardando específico

--- src/main/java/dev/unscrud/mudi/controller/HomeController.java
+++ src/main/java/dev/unscrud/mudi/controller/HomeController.java
@@ -3,22 +3,33 @@ package dev.unscrud.mudi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import dev.unscrud.mudi.model.Pedido;
+import dev.unscrud.mudi.model.StatusPedido;
 import dev.unscrud.mudi.repository.PedidoRepository;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
     @Autowired
     private PedidoRepository pedidoRepository;
     
-    @GetMapping("/home")
+    @GetMapping
     public ModelAndView home(Model model) {
         List<Pedido> pedidos = (List<Pedido>) pedidoRepository.findAll();
+        
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("pedidos",pedidos);
+        
+        return mv;
+    }
+    
+    @GetMapping("/aguardando")
+    public ModelAndView aguardando(Model model) {
+        List<Pedido> pedidos = (List<Pedido>) pedidoRepository.findByStatusPedido(StatusPedido.AGUARDANDO);
         
         ModelAndView mv = new ModelAndView("home");
         mv.addObject("pedidos",pedidos);
