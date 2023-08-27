f19ee019c0493159746fa122f48a5f6e6cd29f02
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-27 19:20:42 -0300

    login via jdbc (trecho comentado responsável por criar usuários no banco)

--- src/main/java/dev/unscrud/mudi/WebSecurityConfig.java
+++ src/main/java/dev/unscrud/mudi/WebSecurityConfig.java
@@ -1,14 +1,17 @@
 package dev.unscrud.mudi;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -16,6 +19,9 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    
+    @Autowired
+    private DataSource dataSource;
     
     /**
      * Foi necessário o uso do MvcRequestMatcher ao utilizar as versões 20 do java
@@ -32,7 +38,6 @@ public class WebSecurityConfig {
      * @return
      * @throws Exception 
      */
-    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, 
             MvcRequestMatcher.Builder mvc) throws Exception {
@@ -49,6 +54,15 @@ public class WebSecurityConfig {
         
         return http.build();
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
+            .authoritiesByUsernameQuery("SELECT username, authority FROM authorities WHERE username = ?")
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }
     
     @Scope("prototype")
     @Bean
@@ -57,14 +71,28 @@ public class WebSecurityConfig {
     }
     
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-           User.withDefaultPasswordEncoder()
-                  .username("user")
-                  .password("pwd")
-                  .roles("USER")
-                  .build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    UserDetailsManager users(DataSource dataSource){
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("pwd"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("pwd"))
+//                .roles("USER", "ADMIN")
+//                .build();
+        
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
 
-        return new InMemoryUserDetailsManager(user);
-    }    
+//        users.createUser(user);
+//        users.createUser(admin);
+
+        return users;
+    }
 }
