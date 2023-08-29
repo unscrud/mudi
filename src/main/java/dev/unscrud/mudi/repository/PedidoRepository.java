4938d51cf98b4604bd55c8f6b62455692c05527e
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-29 18:22:03 -0300

    buscando pedidos por usuário e por status de um usuário

--- src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
+++ src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
@@ -4,10 +4,16 @@ import dev.unscrud.mudi.model.Pedido;
 import dev.unscrud.mudi.model.StatusPedido;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, String>{
 
-    public List<Pedido> findByStatusPedido(StatusPedido statusPedido);
+    @Query("SELECT p FROM Pedido p  JOIN p.user u WHERE u.username = :username AND p.statusPedido = :statusPedido")
+    public List<Pedido> findByStatusPedido( @Param("statusPedido") StatusPedido statusPedido, @Param("username") String username);
+
+    @Query("SELECT p FROM Pedido p  JOIN p.user u WHERE u.username = :username")
+    public List<Pedido> findAllByUser(@Param("username") String username);
 }
