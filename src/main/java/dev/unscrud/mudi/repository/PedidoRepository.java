51ccbca60ba6007d268bd635347cf087199a6b83
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-31 20:19:05 -0300

    usando o cache no findByStatusPedido

--- src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
+++ src/main/java/dev/unscrud/mudi/repository/PedidoRepository.java
@@ -3,7 +3,8 @@ package dev.unscrud.mudi.repository;
 import dev.unscrud.mudi.model.Pedido;
 import dev.unscrud.mudi.model.StatusPedido;
 import java.util.List;
-import org.springframework.data.domain.Sort;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +12,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, String>{
-    public List<Pedido> findByStatusPedido(StatusPedido statusPedido, Sort sort);
-         
+    @Cacheable("pedidosPorStatus")
+    public List<Pedido> findByStatusPedido(StatusPedido statusPedido, Pageable paginacao);
+
     @Query("SELECT p FROM Pedido p  JOIN p.user u WHERE u.username = :username AND p.statusPedido = :statusPedido")
     public List<Pedido> findByStatusPedidoAndUser( @Param("statusPedido") StatusPedido statusPedido, @Param("username") String username);
 
