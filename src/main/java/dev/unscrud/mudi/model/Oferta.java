ff5250d86ec1b0667daa477d1a9c739bad77cf34
Daniel Carvalho <daniel.devop@gmail.com>
2023-09-05 20:53:13 -0300

    modelo para representar uma oferta

---
+++ src/main/java/dev/unscrud/mudi/model/Oferta.java
@@ -0,0 +1,64 @@
+package dev.unscrud.mudi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Oferta {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
+    private BigDecimal valor;
+    private LocalDate dataDaEntrega;
+    private String comentario;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Pedido pedido;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataDaEntrega() {
+        return dataDaEntrega;
+    }
+
+    public void setDataDaEntrega(LocalDate dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}
