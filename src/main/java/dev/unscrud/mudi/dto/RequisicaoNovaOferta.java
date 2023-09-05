0a8446933b12b3473628aeaff7af455db5a81f8a
Daniel Carvalho <daniel.devop@gmail.com>
2023-09-05 20:42:36 -0300

    dto dos dados da criação de uma oferta

---
+++ src/main/java/dev/unscrud/mudi/dto/RequisicaoNovaOferta.java
@@ -0,0 +1,40 @@
+package dev.unscrud.mudi.dto;
+
+class RequisicaoNovaOferta {
+    private String pedidoUuid;
+    private String valor;
+    private String dataDaEntrega;
+    private String comentario;
+
+    public String getPedidoUuid() {
+        return pedidoUuid;
+    }
+
+    public void setPedidoUuid(String pedidoUuid) {
+        this.pedidoUuid = pedidoUuid;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getDataDaEntrega() {
+        return dataDaEntrega;
+    }
+
+    public void setDataDaEntrega(String dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+}
