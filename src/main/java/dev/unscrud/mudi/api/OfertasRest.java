90fef7344125cd269501db26f5cd0cd42227efcd
Daniel Carvalho <daniel.devop@gmail.com>
2023-09-08 11:01:31 -0300

    api de criação de oferta

--- src/main/java/dev/unscrud/mudi/api/OfertasRest.java
+++ src/main/java/dev/unscrud/mudi/api/OfertasRest.java
@@ -6,30 +6,38 @@ import dev.unscrud.mudi.model.Pedido;
 import dev.unscrud.mudi.repository.PedidoRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ofertas")
 public class OfertasRest {
-    
     @Autowired
     private PedidoRepository pedidoRepository;
     
-    public Oferta criarOferta(RequisicaoNovaOferta requisicao) {
-        Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoUuid());
-        
-        if(!pedidoBuscado.isPresent()){
-            return null;
+    @PostMapping
+    public ResponseEntity<Oferta> criarOferta(@RequestBody RequisicaoNovaOferta requisicao) {
+        try {
+            Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoUuid());
+
+            if(!pedidoBuscado.isPresent()){
+                return null;
+            }
+
+
+            Pedido pedido = pedidoBuscado.get();
+            Oferta nova = requisicao.toOferta(pedido);
+            pedido.getOfertas().add(nova);
+
+            pedidoRepository.save(pedido);
+
+            return new ResponseEntity<>(nova, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        
-        
-        Pedido pedido = pedidoBuscado.get();
-        Oferta nova = requisicao.toOferta(pedido);
-        pedido.getOfertas().add(nova);
-        
-        pedidoRepository.save(pedido);
-        
-        return nova;
     }
 }
