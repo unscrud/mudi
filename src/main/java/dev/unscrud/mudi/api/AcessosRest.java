89319bc313ff21f742309a1bd6ed21ea5936e827
Daniel Carvalho <daniel.devop@gmail.com>
2023-09-09 16:12:08 -0300

    obtendo acessos via api

--- src/main/java/dev/unscrud/mudi/api/AcessosRest.java
+++ src/main/java/dev/unscrud/mudi/api/AcessosRest.java
@@ -1,5 +1,17 @@
 package dev.unscrud.mudi.api;
 
+import dev.unscrud.mudi.interceptor.InterceptadorDeAcessos;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/acessos")
+@RestController
 public class AcessosRest {
     
+    @GetMapping
+    public List<InterceptadorDeAcessos.Acesso> getAcessos(){
+        return InterceptadorDeAcessos.acessos;
+    }
 }
